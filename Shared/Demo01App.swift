75fedeb1f5d6a32dbd28f5398c33b85f58a388d8
wangxintao2002 <2454381779@qq.com>
2021-07-19 19:51:35 -0700

    first commit

---
+++ Shared/Demo01App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo01App.swift
+//  Shared
+//
+//  Created by Scott Travis on 2021/7/19.
+//
+
+import SwiftUI
+
+@main
+struct Demo01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
